35dc16b0ab0eed6694cbf882af38dd85d2647537
MarvinXu <272077995@qq.com>
2022-03-13 02:27:50 +0800

    add cssvar demo

---
+++ src/components/CssVar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const el = ref(null)
+const size = useCssVar('--size', el)
+
+function increase() {
+  const v = parseInt(size.value) + 1
+  size.value = `${v}px`
+}
+</script>
+<template>
+  <div
+    ref="el"
+    bg="blue-400"
+    w-10
+    style="--size: 12px;"
+    h="$size"
+  />
+  <button block border @click="increase()">
+    add
+  </button>
+</template>
